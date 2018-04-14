9955942084ebb6ce750a98f5f0144a7291f50f68
quentin <quentin.ronzeau@gmail.com>
2018-04-14 12:13:48 +0200

    First commit: get list of process, display  & record on a file

---
+++ Process/main.c
@@ -0,0 +1,81 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <psapi.h>
+
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+
+FILE *recordFile;
+
+void PrintProcessNameAndID( DWORD processID )
+{
+    TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+
+    // Get a handle to the process.
+
+    HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
+                                   PROCESS_VM_READ,
+                                   FALSE, processID );
+
+    // Get the process name.
+
+    if (NULL != hProcess )
+    {
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod), 
+             &cbNeeded) )
+        {
+            GetModuleBaseName( hProcess, hMod, szProcessName, 
+                               sizeof(szProcessName)/sizeof(TCHAR) );
+        }
+    }
+
+    // Print the process name and identifier.
+
+    _tprintf( TEXT("%s  (PID: %u)\n"), szProcessName, processID );
+    fprintf(recordFile, TEXT("%s  (PID: %u)\n"), szProcessName, processID);
+
+    // Release the handle to the process.
+
+    CloseHandle( hProcess );
+}
+
+int main( void )
+{
+    // Get the list of process identifiers.
+
+	recordFile = fopen("listProcess.txt","w");
+
+    DWORD aProcesses[1024], cbNeeded, cProcesses;
+    unsigned int i;
+
+    if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+    {
+        return 1;
+    }
+
+
+    // Calculate how many process identifiers were returned.
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    // Print the name and process identifier for each process.
+
+	    for ( i = 0; i < cProcesses; i++ )
+	    {
+	        if( aProcesses[i] != 0 )
+	        {
+	            PrintProcessNameAndID( aProcesses[i] );
+	        }
+	    }
+
+	fclose(recordFile);
+
+	while(1);
+
+    return 0;
+}
